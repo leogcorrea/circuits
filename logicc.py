fix autograd calculation

logicc.py
@@ -13,7 +13,7 @@ class Layer(nn.Module):
     def __init__(self, id, matrix):
         super().__init__()
         self.id = id
-        self.weight = nn.Parameter(matrix, requires_grad=True)
+        self.weight = nn.Parameter(matrix) #, requires_grad=True)
 
 
 class InputLayer(Layer):
@@ -27,20 +27,20 @@ class InputLayer(Layer):
         #self.linear_transform.weight = nn.Parameter(self.weight)
   
         #self.register_buffer("negated", negated)
-        self.negated = nn.Parameter(negated.float(), requires_grad=True)
+        self.negated = nn.Parameter(negated.float()) #, requires_grad=True)
         #self.register_buffer("input_mask", input_mask)
-        self.input_mask = nn.Parameter(input_mask, requires_grad=True)
+        self.input_mask = nn.Parameter(input_mask) #, requires_grad=True)
         #self.register_buffer("_mask", input_mask.t())
-        self._mask = nn.Parameter(input_mask.t(), requires_grad=True)
+        self._mask = nn.Parameter(input_mask.t()) #, requires_grad=True)
         #self.register_buffer("gains", None)
-        self.gains = nn.Parameter(None, requires_grad=True)
+        self.gains = nn.Parameter(None) #, requires_grad=True)
         self.gain_set = None
         self.set_gains(gains, [])
         self.gain_set = False
     
     def set_gains(self, gains, surrogate):
         """ Gains (weights) applied by the linear transform on the inputs """
-        self.gains = nn.Parameter(gains, requires_grad=True) # store gains for future reference
+        self.gains = nn.Parameter(gains) #, requires_grad=True) # store gains for future reference
         xgains = torch.matmul(self.input_mask, gains) # map gain values to internal nodes organization
         sel = self.negated - xgains # build a negation mask for gains
         # select those node acting as surrogate facts for annotated disjunctions 
@@ -56,7 +56,7 @@ class InputLayer(Layer):
     
     def set_negated(self, value):
         """ Mask to obtain the negation of input literal according to the circuit setup """
-        self.negated = nn.Parameter(value.float(), requires_grad=True)
+        self.negated = nn.Parameter(value.float()) #, requires_grad=True)
 
     
     def forward(self, input):
@@ -87,7 +87,7 @@ class AndLayer(Layer):
         #self.linear_transform = nn.Linear(in_features=matrix.size(dim=1), out_features=matrix.size(dim=0), bias=False)
         #self.linear_transform.weight = nn.Parameter(matrix)
         #self.register_buffer("_mask", (matrix == 0))
-        self._mask = nn.Parameter((matrix==0).float(), requires_grad=True)
+        self._mask = nn.Parameter((matrix==0).float()) #, requires_grad=True)
 
     def forward(self, input):
         #return self.linear_transform(input)
@@ -336,7 +336,7 @@ class LogicCircuit(nn.Sequential):
         for layer in layers:
             self.append(layer)
         self.nliterals = nliterals
-        self.probnorm = torch.tensor(1.0, requires_grad=True)
+        self.probnorm = torch.tensor(1.0) #, requires_grad=True)
         self._device = torch.device("cpu")
 
     def to(self, device):
@@ -364,7 +364,7 @@ class LogicCircuit(nn.Sequential):
         if len(self.layers) == 0:
             raise IndexError("No input layer defined")
         self.layers[0].set_gains(value, surrogate)
-        ones = torch.ones(1, self.get_input_size(), requires_grad=True).to(self._device)
+        ones = torch.ones(1, self.get_input_size()).to(self._device) #, requires_grad=True).to(self._device)
         self.probnorm = self(ones)
    
     def get_input_size(self):
@@ -486,7 +486,7 @@ def test_configurations(filename = 'simple_w_constraint_opt'):
 
 def make_query(expr, symbols):
     """ Helper function to make a query given a expression and a dict of symbols """
-    return torch.tensor(symbols[expr]-1 if not 'not' in expr else -symbols[expr.replace('not', '').lstrip()]+1)
+    return torch.tensor([symbols[expr]-1] if not 'not' in expr else [-symbols[expr.replace('not', '').lstrip()]+1])
 
 
 def test_probabilities(c2d_executable):
@@ -501,7 +501,7 @@ def test_probabilities(c2d_executable):
     print("Circuit being tested: ", filename + '.nnf')
     
     #lit2idx = lambda lit: lit-1
-    sym2lit = lambda sym: torch.tensor([symbols[sym]-1] if not 'not' in sym else [-symbols[sym.replace('not', '').lstrip()]+1])
+    sym2lit = lambda sym: make_query(sym, symbols)
     #sym2idx = lambda sym: lit2idx(sym2lit(sym))
 
     probs = torch.ones(circuit.nliterals)

sum.py
@@ -79,15 +79,18 @@ def operate(nn_model, pc_model, query_builder, device, data_loader, optimizer, e
     else:
         nn_model.eval()
     test_loss = 0
+    #loss = 0
     correct = 0
     n = 19    
     queries = torch.zeros(n, 1)
     for k in range(0, n):
         queries[k] = query_builder( f"add({k})" )
+    half_batch = data_loader.batch_size // 2
     s = len(data_loader.dataset)//2
-    m = 20
     b = 1
-    probs = torch.ones(500, circuit.nliterals).to(device)
+    batches = len(data_loader.dataset) // data_loader.batch_size
+
+    probs = torch.ones(half_batch, circuit.nliterals).to(device)
 
     ###f = torch.vmap(pc_model.query)
 
@@ -95,34 +98,40 @@ def operate(nn_model, pc_model, query_builder, device, data_loader, optimizer, e
         for batch_idx, (data, target) in enumerate(data_loader):
             if train_mode:
                 optimizer.zero_grad()
-            start = time.time()
-            print("\rBatch {} of {}".format(b, 10))
+            #start = time.time()
+            print("\rBatch {} of {}".format(b, batches))
             b+=1
             data, target = data.to(device), target.to(device)
             output = nn_model(data)
 
             h = output.size(dim=0)//2
 
-            pred = torch.zeros(h, requires_grad=True).to(device)
+            #pred = torch.zeros(h, n).to(device)
+            #tgt = torch.add(target[:h], target[h:]).to(device)
+            pred = torch.empty(h).to(device)
             tgt = torch.add(target[:h], target[h:]).float().to(device)
-            tgt.requires_grad = True
-            probs[:, 0:m] = torch.concat((output[:h], output[h:]), dim = 1)
+            probs[:, 0:(n+1)] = torch.concat((output[:h], output[h:]), dim = 1)
 
-            for i, (p, _) in enumerate(zip(probs, tgt)): 
-                q = torch.zeros(n)
+            for i, p in enumerate(probs): 
+                q = torch.zeros(n).to(device)
 
                 pc_model.set_input_weights(p)
 
                 for k in range(0, n):
                    q[k] = pc_model.query(queries[k])
+                #   pred[i, k] = pc_model.query(queries[k])
                 
                 pred[i] = q.argmax(keepdim=True)
 
-            elapsed = time.time() - start
-            print("Batch elapsed time:", elapsed)
-
-            #loss += F.nll_loss(pred, tgt, reduction='sum').item()  # sum up batch loss
+            #elapsed = time.time() - start
+            #print("Batch elapsed time:", elapsed)
+            tgt.requires_grad = True
+            pred.requires_grad = True
+            
+            #correct += tgt.eq(pred.view_as(tgt)).sum().item()
             loss = F.cross_entropy(pred, tgt)
+            #loss = F.nll_loss(pred, tgt) #, reduction='sum').item()  # sum up batch loss
+         
 
             if train_mode:
                 loss.backward()
@@ -130,7 +139,7 @@ def operate(nn_model, pc_model, query_builder, device, data_loader, optimizer, e
             else:
                 test_loss += loss
 
-            correct += tgt.eq(pred.view_as(tgt)).sum().item()
+            #correct += tgt.eq(pred.view_as(tgt)).sum().item()
  
         print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
             epoch, (batch_idx-1) * len(data), len(data_loader.dataset),
@@ -159,8 +168,8 @@ if __name__ == '__main__':
     else:
         device = torch.device("cpu")
 
-    train_kwargs = {'batch_size': 64}
-    test_kwargs = {'batch_size': 1000}
+    train_kwargs = {'batch_size': 500}
+    test_kwargs = {'batch_size': 50}
 
     transform=transforms.Compose([
         transforms.ToTensor(),